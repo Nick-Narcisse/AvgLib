bb0aa814bf28e0539e43f2d643a40e54608f7692
Pt-luck <nickn.6012@gmail.com>
2023-02-15 14:51:54 -0500

    AverageLib

---
+++ vs4win/Chapter04/MyAvgLib/Average.cs
@@ -0,0 +1,15 @@
+﻿namespace MyAvgLib
+{
+    public static class Average
+    {
+        public static double Avg(double a, double b)
+        {
+            return (a + b) / 2;
+        }
+
+        public static double Avg(double a, double b, double c)
+        {
+            return (a + b + c) / 3;
+        }
+    }
+}
---
+++ vs4win/Chapter04/MyAvgLibTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using MyAvgLib;
+
+namespace MyAvgLibTest
+{
+    public class AverageTests
+    {
+        [Fact]
+        public void TestAverageOfTwoNumbers()
+        {
+            double a = 10.5;
+            double b = 7.2;
+            double expected = 8.85;
+
+            double actual = Average.Avg(a, b);
+
+            Assert.Equal(expected, actual, 2);
+        }
+
+        [Fact]
+        public void TestAverageOfThreeNumbers()
+        {
+            double a = 2.1;
+            double b = 4.7;
+            double c = 6.3;
+            double expected = 4.367;
+
+            double actual = Average.Avg(a, b, c);
+
+            Assert.Equal(expected, actual, 3);
+        }
+    }
+}
